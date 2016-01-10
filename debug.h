9a1ecce78942cc592545ef2454438a1b7d8e4f1a
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2016-01-10 17:56:36 +0300

    ifdef DBG define INFO

--- debug.h
+++ debug.h
@@ -4,6 +4,10 @@ extern char *whoami;
 
 #define PRNT(level, str, ...) fprintf(stderr, "%6s: [%5s] " __FILE__ ": "  str "\n", whoami, level, ##__VA_ARGS__)
 
+#ifdef DBG
 #define INFO(...) PRNT("INFO", ##__VA_ARGS__)
+#else
+#define INFO(...)
+#endif
 #define  ERR(...) PRNT("ERR", ##__VA_ARGS__)
 
